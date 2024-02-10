03d9b962758cf4b0178a53fba8dc4f42861a66f3
RK <kanno.media808@gmail.com>
2024-02-10 09:17:47 -0800

    Add custom button and make view to use autolayout.

--- LifecycleFunctions/CustomChildVC.swift
+++ LifecycleFunctions/CustomChildVC.swift
@@ -17,18 +17,46 @@ class CustomChildVC: UIViewController {
         label.text = "I'm a CustomChildVC!"
         return label
     }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Tap me", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.layer.shadowRadius = 5
+        button.layer.shadowOffset = CGSize(width: 3, height: 3)
+        button.addTarget(
+            self,
+            action: #selector(didTapButton),
+            for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .systemTeal
         view.addSubview(label)
         NSLayoutConstraint.activate([
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
     }
     
     override func didMove(toParent parent: UIViewController?) {
         super.didMove(toParent: parent)
+    }
+    
+    @objc private func didTapButton() {
+        print("CustomChildVC: \(#function)")
     }
 }
